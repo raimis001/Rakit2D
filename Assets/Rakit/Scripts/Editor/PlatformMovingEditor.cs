3e012709727703a37984afdcbedb4b7276a7d6a8
Raimonds <raimis0001@gmail.com>
2019-08-11 14:51:40 +0300

    Moving platform

--- Assets/Rakit/Scripts/Editor/PlatformMovingEditor.cs
+++ Assets/Rakit/Scripts/Editor/PlatformMovingEditor.cs
@@ -21,7 +21,75 @@ public class PlatformMovingEditor : Editor
 	public override void OnInspectorGUI()
 	{
 
-		base.OnInspectorGUI();
+		//base.OnInspectorGUI();
+
+		EditorGUI.BeginChangeCheck();
+		PlatformMovingKind kind = (PlatformMovingKind)EditorGUILayout.EnumPopup("Looping", platform.kind);
+		if (EditorGUI.EndChangeCheck())
+		{
+			Undo.RecordObject(target, "Changed Moving Platform type");
+			platform.kind = kind;
+		}
+
+		EditorGUI.BeginChangeCheck();
+		float newSpeed = EditorGUILayout.FloatField("Speed", platform.speed);
+		if (EditorGUI.EndChangeCheck())
+		{
+			Undo.RecordObject(target, "Changed Speed");
+			platform.speed = newSpeed;
+		}
+
+		EditorGUI.BeginChangeCheck();
+		platform.platform = EditorGUILayout.ObjectField("Platform", platform.platform, typeof(Transform), true) as Transform;
+		if (EditorGUI.EndChangeCheck())
+			Undo.RecordObject(target, "Changed platform");
+
+		EditorGUILayout.Separator();
+		EditorGUILayout.LabelField("Nodes");
+
+		EditorGUILayout.BeginVertical("box");
+
+		EditorGUILayout.LabelField("Node 0");
+
+		EditorGUI.BeginChangeCheck();
+		float fDelay = EditorGUILayout.FloatField("Wait Time", platform.zeroDelay);
+		if (EditorGUI.EndChangeCheck())
+		{
+			Undo.RecordObject(target, "Changed zero delay");
+			platform.zeroDelay = fDelay;
+		}
+
+		EditorGUILayout.EndVertical();
+
+		int delete = -1;
+		for (int i = 0; i < platform.nodes.Count; i++)
+		{
+			EditorGUI.BeginChangeCheck();
+			PlatformNode node = platform.nodes[i];
+
+			EditorGUILayout.BeginVertical("box");
+
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("Node " + (i + 1).ToString() + "   " + string.Format("x:{0:0.000} y:{1:0.000}", node.position.x, node.position.y));
+			if (GUILayout.Button("Delete", GUILayout.Width(100)))
+			{
+				
+				delete = i;
+			}
+
+			EditorGUILayout.EndHorizontal();
+
+			float wDelay = EditorGUILayout.FloatField("Wait Time", node.waitOnNode);
+
+			EditorGUILayout.EndVertical();
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				Undo.RecordObject(target, "Changed node " + i);
+				node.waitOnNode = wDelay;
+			}
+		}
+
 
 		Vector3 last = platform.nodes.Count > 0 ? platform.nodes[platform.nodes.Count - 1].position : Vector3.zero;
 		if (GUILayout.Button("Add Node"))
@@ -34,8 +102,13 @@ public class PlatformMovingEditor : Editor
 		testSlider = EditorGUILayout.Slider("Preview position", testSlider, 0.0f, 1.0f);
 		PreviewPlatform();
 
+		if (delete > -1)
+		{
+			Undo.RecordObject(target, "Delete node " + delete);
+			platform.nodes.RemoveAt(delete);
+		}
+		EditorUtility.SetDirty(target);
 	}
-
 	private void OnSceneGUI()
 	{
 
@@ -54,6 +127,8 @@ public class PlatformMovingEditor : Editor
 			lastNodePos = nPos;
 		}
 		PreviewPlatform();
+		EditorUtility.SetDirty(target);
+
 	}
 	void PreviewPlatform()
 	{
@@ -61,7 +136,7 @@ public class PlatformMovingEditor : Editor
 			return;
 
 		platform.MovePlatform(testSlider);
-	
+
 		SceneView.RepaintAll();
 	}
 }
--- Assets/Rakit/Scripts/PlatformMoving.cs
+++ Assets/Rakit/Scripts/PlatformMoving.cs
@@ -1,32 +1,110 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum PlatformMovingKind
+{
+	Once, PinPong, Loop
+}
 
 [System.Serializable]
 public class PlatformNode
 {
 	public Vector3 position;
+	public float waitOnNode = 0;
 }
 public class PlatformMoving : Interact
 {
 	public Transform platform;
+	public float speed = 1;
+	public PlatformMovingKind kind;
+	public List<PlatformNode> nodes = new List<PlatformNode>();
 
-	public List<PlatformNode> nodes = new List<PlatformNode>(); 
+	public float zeroDelay;
+
+	int currentNode = 0;
+	Vector3[] localNodes;
+	bool isMoving;
 
 	public void StartMove(Interact parent)
 	{
-
+		if (isMoving)
+			return;
+		StartCoroutine(Move());
 	}
 
 	public void StopMove(Interact parent)
 	{
+		if (!isMoving)
+			return;
+		StopAllCoroutines();
+		isMoving = false;
+	}
+	private void Start()
+	{
+		int nodesCount = nodes.Count;
+		localNodes = new Vector3[nodesCount + 1];
+		localNodes[0] = Vector3.zero;
+		for (int i = 0; i < nodesCount; i++)
+			localNodes[i + 1] = nodes[i].position;
 
+		
 	}
 
+	IEnumerator Move(float delay = 0)
+	{
+		isMoving = true;
+		if (delay > 0)
+			yield return new WaitForSeconds(delay);
+
+		//Debug.Log("Start move:" + currentNode);
+
+		int startNode = currentNode;
+		int nodesCount = startNode == 0 ? nodes.Count : 0;
+		int sign = startNode == 0 ? 1 : -1;
+
+		while (startNode != nodesCount)
+		{
+			Vector3 target = localNodes[startNode + sign];
+			platform.localPosition = Vector3.MoveTowards(platform.localPosition, target , Time.deltaTime * speed);
+			if (Vector3.Distance(platform.localPosition, target) < 0.001f)
+			{
+				int onNode = sign > 0 ? startNode : startNode - 2;
+				//Debug.Log("Node reach:" + onNode);
+
+				if (onNode >= 0 && nodes[onNode].waitOnNode > 0)
+					yield return new WaitForSeconds(nodes[onNode].waitOnNode);
+
+				startNode += sign;
+				currentNode = startNode;
+			}
+			yield return null;
+		}
+		//Debug.Log("End move:" + currentNode);
+
+		if (kind == PlatformMovingKind.Once)
+		{
+			isMoving = false;
+			yield break;
+		}
+
+		if (kind == PlatformMovingKind.PinPong)
+		{
+			if (currentNode == 0)
+			{
+				isMoving = false;
+				yield break;
+			}
+		}
+
+		if (currentNode == 0 && zeroDelay > 0)
+			yield return new WaitForSeconds(zeroDelay);
+
+		StartCoroutine(Move());
+	}
+
+#if UNITY_EDITOR
 	public void MovePlatform(float distance)
 	{
-		if (Application.isPlaying)
-			return;
 
 		int nodesCount = nodes.Count;
 		if (nodesCount < 1)
@@ -43,8 +121,8 @@ public class PlatformMoving : Interact
 		}
 
 		float step = 1.0f / nodesCount;
-
 		int starting = Mathf.FloorToInt(distance / step);
+
 		if (starting > nodesCount - 1)
 		{
 			platform.localPosition = localNodes[localNodes.Length - 1];
@@ -57,5 +135,5 @@ public class PlatformMoving : Interact
 		platform.localPosition = localPos;
 
 	}
-
+#endif
 }

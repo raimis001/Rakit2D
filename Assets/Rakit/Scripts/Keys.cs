d49fee3f077ce8e95e17ee3bf48a39a2795a5b45
Raimonds <raimis0001@gmail.com>
2019-08-28 13:40:04 +0300

    Keybind refactoring

---
+++ Assets/Rakit/Scripts/Keys.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum KeyType
+{
+  Key, Button, Axis, Mouse
+}
+public enum KeyKind
+{
+  Down, Up, Key
+}
+[System.Serializable]
+public struct KeyBind
+{
+  public KeyType type;
+  public string inputName;
+  public float axisCheck;
+  public KeyCode key;
+  public bool isShift;
+  public bool isControl;
+  public KeyKind kind;
+  public bool IsKey => CheckKey();
+
+  private bool CheckShift()
+  {
+    return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+  }
+  private bool CheckControl()
+  {
+    return Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) ||
+          Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand);
+  }
+  private bool CheckKey()
+  {
+    if (type == KeyType.Button)
+    {
+      switch (kind)
+      {
+        case KeyKind.Down:
+          if (!Input.GetButtonDown(inputName))
+            return false;
+          break;
+        case KeyKind.Up:
+          if (!Input.GetButtonUp(inputName))
+            return false;
+          break;
+        default:
+          if (!Input.GetButton(inputName))
+            return false;
+          break;
+      }
+
+      if (isShift && !CheckShift())
+        return false;
+
+      if (isControl && !CheckControl())
+        return false;
+
+      return true;
+    }
+
+    if (type == KeyType.Axis)
+    {
+
+      if (axisCheck < 0)
+        return Input.GetAxis(inputName) < axisCheck;
+
+      if (axisCheck > 0)
+        return Input.GetAxis(inputName) > axisCheck;
+
+      return Mathf.Abs(Input.GetAxis(inputName)) > 0.1f;
+    }
+
+    if (type == KeyType.Mouse)
+    {
+      int mouse = 0;
+      switch (key)
+      {
+        case KeyCode.Mouse0:
+          mouse = 0;
+          break;
+        case KeyCode.Mouse1:
+          mouse = 2;
+          break;
+        case KeyCode.Mouse2:
+          mouse = 1;
+          break;
+      }
+
+      switch (kind)
+      {
+        case KeyKind.Down:
+          if (!Input.GetMouseButtonDown(mouse))
+            return false;
+          break;
+        case KeyKind.Up:
+          if (!Input.GetMouseButtonUp(mouse))
+            return false;
+          break;
+        default:
+          if (!Input.GetMouseButton(mouse))
+            return false;
+          break;
+      }
+    }
+
+    if (key != KeyCode.None)
+    {
+      switch (kind)
+      {
+        case KeyKind.Down:
+          if (!Input.GetKeyDown(key))
+            return false;
+          break;
+        case KeyKind.Up:
+          if (!Input.GetKeyUp(key))
+            return false;
+          break;
+        default:
+          if (!Input.GetKey(key))
+            return false;
+          break;
+      }
+    }
+    else
+    {
+      switch (kind)
+      {
+        case KeyKind.Down:
+        case KeyKind.Up:
+          if (!Input.anyKeyDown)
+            return false;
+          break;
+        default:
+          if (!Input.anyKey)
+            return false;
+          break;
+      }
+    }
+
+    if (isShift && !CheckShift())
+      return false;
+
+    if (isControl && !CheckControl())
+      return false;
+
+    return true;
+  }
+}
+
+public class Keys : MonoBehaviour
+{
+
+  public static Keys instance;
+
+  [Header("Key bindings")]
+  public string horizontal = "Horizontal";
+  public KeyBind interactKey = new KeyBind() { type = KeyType.Key, key = KeyCode.E, kind = KeyKind.Down };
+  public KeyBind jumpKey = new KeyBind() { type = KeyType.Button, inputName = "Jump", kind = KeyKind.Down };
+  public KeyBind downKey = new KeyBind() { type = KeyType.Axis, inputName = "Vertical", axisCheck = -0.1f };
+
+  [Header("Dialog choices")]
+  public KeyBind choice1 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha1, kind = KeyKind.Down };
+  public KeyBind choice2 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha2, kind = KeyKind.Down };
+  public KeyBind choice3 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha3, kind = KeyKind.Down };
+
+  [Header("Weapon")]
+  public KeyBind attack = new KeyBind() { type = KeyType.Key, key = KeyCode.None, isControl = true, kind = KeyKind.Down };
+  public KeyBind weapon1 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha1, kind = KeyKind.Down };
+  public KeyBind weapon2 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha1, kind = KeyKind.Down };
+
+  private void Awake()
+  {
+    instance = this;
+  }
+}
--- Assets/Rakit/Scripts/SM.cs
+++ Assets/Rakit/Scripts/SM.cs
@@ -2,196 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum KeyType
-{
-  Key, Button, Axis, Mouse
-}
-public enum KeyKind
-{
-  Down, Up, Key
-}
-[System.Serializable]
-public struct KeyBind
-{
-  public KeyType type;
-  public string inputName;
-  public float axisCheck;
-  public KeyCode key;
-  public bool isShift;
-  public bool isControl;
-  public KeyKind kind;
-  public bool IsKey => CheckKey();
-
-  private bool CheckShift()
-  {
-    return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-  }
-  private bool CheckControl()
-  {
-    return Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) ||
-          Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand);
-  }
-  private bool CheckKey()
-  {
-    if (type == KeyType.Button)
-    {
-      switch (kind)
-      {
-        case KeyKind.Down:
-          if (!Input.GetButtonDown(inputName))
-            return false;
-          break;
-        case KeyKind.Up:
-          if (!Input.GetButtonUp(inputName))
-            return false;
-          break;
-        default:
-          if (!Input.GetButton(inputName))
-            return false;
-          break;
-      }
-
-      if (isShift && !CheckShift())
-        return false;
-
-      if (isControl && !CheckControl())
-        return false;
-
-      return true;
-    }
-
-    if (type == KeyType.Axis)
-    {
-
-      if (axisCheck < 0)
-        return Input.GetAxis(inputName) < axisCheck;
-
-      if (axisCheck > 0)
-        return Input.GetAxis(inputName) > axisCheck;
-
-      return Mathf.Abs(Input.GetAxis(inputName)) > 0.1f;
-    }
-
-    if (type == KeyType.Mouse)
-    {
-      int mouse = 0;
-      switch (key)
-      {
-        case KeyCode.Mouse0:
-          mouse = 0;
-          break;
-        case KeyCode.Mouse1:
-          mouse = 2;
-          break;
-        case KeyCode.Mouse2:
-          mouse = 1;
-          break;
-      }
-
-      switch (kind)
-      {
-        case KeyKind.Down:
-          if (!Input.GetMouseButtonDown(mouse))
-            return false;
-          break;
-        case KeyKind.Up:
-          if (!Input.GetMouseButtonUp(mouse))
-            return false;
-          break;
-        default:
-          if (!Input.GetMouseButton(mouse))
-            return false;
-          break;
-      }
-    }
-
-    if (key != KeyCode.None)
-    {
-      switch (kind)
-      {
-        case KeyKind.Down:
-          if (!Input.GetKeyDown(key))
-            return false;
-          break;
-        case KeyKind.Up:
-          if (!Input.GetKeyUp(key))
-            return false;
-          break;
-        default:
-          if (!Input.GetKey(key))
-            return false;
-          break;
-      }
-    }
-    else
-    {
-      switch (kind)
-      {
-        case KeyKind.Down:
-        case KeyKind.Up:
-          if (!Input.anyKeyDown)
-            return false;
-          break;
-        default:
-          if (!Input.anyKey)
-            return false;
-          break;
-      }
-    }
-
-    if (isShift && !CheckShift())
-      return false;
-
-    if (isControl && !CheckControl())
-      return false;
-
-    return true;
-  }
-}
 
 public class SM : MonoBehaviour
 {
+  public static SM instance;
+
   public static Inventory inventory;
   public static Player player;
 
-  public static SM instance;
+  #region KEYS
+  public static float keyMove => Input.GetAxis(Keys.instance.horizontal);
+  public static bool keyInteract => Keys.instance.interactKey.IsKey;
+  public static bool keyJump => Keys.instance.jumpKey.IsKey;
+  public static bool keyDown => Keys.instance.downKey.IsKey;
 
-  [Header("Key bindings")]
-  public string horizontal = "Horizontal";
-  public KeyBind interactKey = new KeyBind() { type = KeyType.Key, key = KeyCode.E, kind = KeyKind.Down };
-  public KeyBind jumpKey = new KeyBind() { type = KeyType.Button, inputName = "Jump", kind = KeyKind.Down };
-  public KeyBind downKey = new KeyBind() { type = KeyType.Axis, inputName = "Vertical", axisCheck = -0.1f };
+  public static bool dialogChoice1 => Keys.instance.choice1.IsKey;
+  public static bool dialogChoice2 => Keys.instance.choice2.IsKey;
+  public static bool dialogChoice3 => Keys.instance.choice3.IsKey;
 
-  [Header("Dialog choices")]
-  public KeyBind choice1 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha1, kind = KeyKind.Down };
-  public KeyBind choice2 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha2, kind = KeyKind.Down };
-  public KeyBind choice3 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha3, kind = KeyKind.Down };
+  public static bool keyAttack => Keys.instance.attack.IsKey;
+  public static bool keyWeapon1 => Keys.instance.weapon1.IsKey;
+  public static bool keyWeapon2 => Keys.instance.weapon2.IsKey;
+  #endregion
 
-  [Header("Weapon")]
-  public KeyBind attack = new KeyBind() { type = KeyType.Key, key = KeyCode.None, isControl = true, kind = KeyKind.Down };
-  public KeyBind weapon1 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha1, kind = KeyKind.Down };
-  public KeyBind weapon2 = new KeyBind() { type = KeyType.Key, key = KeyCode.Alpha1, kind = KeyKind.Down };
+  public static bool dialogOpened;
+
+  public delegate void LivesChange();
+  public static event LivesChange OnLivesChange;
+
+  public static int Lives = 3;
+  public static float Hp = 1;
+
+  public int maxLives = 3;
+
+  public static void SetHp(float hp)
+  {
+    Hp -= hp;
+    if (Hp > 0)
+    {
+      OnLivesChange.Invoke();
+      return;
+    }
+
+    Lives--;
+    if (Lives < 0)
+    {
+      //TODO ded
+      OnLivesChange.Invoke();
+      return;
+    }
+
+    Hp = 1;
+    OnLivesChange.Invoke();
+
+  }
 
   private void Awake()
   {
     instance = this;
+    Lives = maxLives;
   }
-
-  public static float keyMove => Input.GetAxis(instance.horizontal);
-  public static bool keyInteract => instance.interactKey.IsKey;
-  public static bool keyJump => instance.jumpKey.IsKey;
-  public static bool keyDown => instance.downKey.IsKey;
-
-  public static bool dialogChoice1 => instance.choice1.IsKey;
-  public static bool dialogChoice2 => instance.choice2.IsKey;
-  public static bool dialogChoice3 => instance.choice3.IsKey;
-
-  public static bool keyAttack => instance.attack.IsKey;
-  public static bool keyWeapon1 => instance.weapon1.IsKey;
-  public static bool keyWeapon2 => instance.weapon2.IsKey;
-
-  public static bool dialogOpened;
-
   public void TestInventory(string itemName, int cnt)
   {
     if (!Inventory.GetDefine(itemName, out InventoryItem item))
